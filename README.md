4343ae3a076dc12c23b1d6d5df4dc68b628e0d08
jeterpan <jeter.costa@gmail.com>
2019-09-11 12:43:29 -0300

    Documentar tema Instalação no README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # modelo_WS_Node
 Modelo de WebService usando Node.js
+
+# instalação
+Configurar as váriaveis de ambiente
+  UV_THREADPOOL_SIZE - quantidade de threads a serem utilizadas entre Node.js e banco de dados
+  WS_MODELO_CONNSTRING - String de conexão com o banco de dados: exemplo: 10.0.7.156/HOM
+  WS_MODELO_BDUSER - Usuario para WS Modelo conectar ao banco de dados
+  WS_MODELO_BDPASSWORD - senha para WS Modelo conectar ao banco de dados
+  WS_MODELO_CORS_ALLOW_ORIGINS - Origens permitidas: exemplo: 'http://localhost:4200'
+
+Revisar os arquivos de configuração
+  ./configs/aplicacao.js
+  ./configs/banco.js
+  ./configs/servidorWeb.js
+  
--- index.js
+++ index.js
@@ -9,22 +9,6 @@
 //  - Docker - preparar para trabalhar em Docker
 //  - Micro serviços - Manter em mente que este é um modelo a ser utilizado como exemplo para vários micro serviços
 
-
-// Instruções de instalação:
-//
-//  Configurar as váriaveis de ambiente
-//   UV_THREADPOOL_SIZE - quantidade de threads a serem utilizadas entre Node.js e banco de dados
-//   WS_MODELO_CONNSTRING - String de conexão com o banco de dados: exemplo: 10.0.7.156/HOM
-//   WS_MODELO_BDUSER - Usuario para WS Modelo conectar ao banco de dados
-//   WS_MODELO_BDPASSWORD - senha para WS Modelo conectar ao banco de dados
-//   WS_MODELO_CORS_ALLOW_ORIGINS - Origens permitidas: exemplo: 'http://localhost:4200'
-
-//  Revisar os arquivos de configuração
-//   ./configs/aplicacao.js
-//   ./configs/banco.js
-//   ./configs/servidorWeb.js
-
-
 // modulos desta aplicacao
 const servidorWeb = require('./services/servidorWeb.js')
 const banco = require('./servicos/banco.js/index.js')
