8c4eddccf96dfe11e9de0a5d7a0ea26f2d0dc65c
JeterPan <41131222+jeterpan@users.noreply.github.com>
2021-03-07 19:03:30 -0300

    Adicionando comentário
    Adicionando comentário

--- configs/banco.js
+++ configs/banco.js
@@ -1,3 +1,8 @@
+// Christpher Jones, Senior Principal Product Manager, indica criar um pool com tamanho fixo
+//  Por este motivo o poolMin e poolMax sao iguais e o poolIncrement é 0 zero
+//  O benefício de ter um Pool com tamanho fixo é evitar que firewalls derrubem conexões inativas durante a
+//   noite (caso o Pool seja com conexões variáveis), e evitar connection storm durante a manhã, quando os usuários chegam e muitos querem conectar
+//   simultaneamente, causando lentidão.
 module.exports = {
     hrPool: {
           user: process.env.WS_MODELO_BDUSER
