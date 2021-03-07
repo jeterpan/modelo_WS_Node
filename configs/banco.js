2bb9ab02b89c6c2d41a6f0a47b2fb1b65dad9db7
JeterPan <41131222+jeterpan@users.noreply.github.com>
2021-03-07 19:17:27 -0300

    Melhorando o comentário
    Melhorando o comentário

--- configs/banco.js
+++ configs/banco.js
@@ -3,6 +3,10 @@
 //  O benefício de ter um Pool com tamanho fixo é evitar que firewalls derrubem conexões inativas durante a
 //   noite (caso o Pool seja com conexões variáveis), e evitar connection storm durante a manhã, quando os usuários chegam e muitos querem conectar
 //   simultaneamente, causando lentidão.
+
+// Conselho: Defina o poolMax com um numero minimo necessario de conexoes maximas, e caso precisar aumentar ele, reveja tambem o numero de threads
+//  do Node (creio que são threads entre Node.js e libUV): parametro: UV_THREADPOOL_SIZE
+// O Christopher disse que tem ferramentas para monitorar isso, procurar a respeito
 module.exports = {
     hrPool: {
           user: process.env.WS_MODELO_BDUSER
